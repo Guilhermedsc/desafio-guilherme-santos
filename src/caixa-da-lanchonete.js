3eecf51e24ef46c715879a556c4d3cd7bab60460
Guilherme <guilhermescod@alu.ufc.br>
2023-08-12 11:45:29 -0300

    3 failed, 27 passed

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,72 +1,102 @@
 class CaixaDaLanchonete {
-    calcularValorDaCompra(formaDePagamento, itens) {
-        // Definir o cardápio com os valores dos itens
-        const cardapio = {
-            cafe: 3.00,
-            chantily: 1.50,
-            suco: 6.20,
-            sanduiche: 6.50,
-            queijo: 2.00,
-            salgado: 7.25,
-            combo1: 9.50,
-            combo2: 7.50,
+    constructor() {
+        // Definição do cardápio com descrição, valor e indicação de ser extra ou não
+        this.cardapio = {
+            cafe: { descricao: 'Café', valor: 3.00, extra: false },
+            chantily: { descricao: 'Chantily (extra do Café)', valor: 1.50, extra: true },
+            suco: { descricao: 'Suco Natural', valor: 6.20, extra: false },
+            sanduiche: { descricao: 'Sanduíche', valor: 6.50, extra: false },
+            queijo: { descricao: 'Queijo (extra do Sanduíche)', valor: 2.00, extra: true },
+            salgado: { descricao: 'Salgado', valor: 7.25, extra: false },
+            combo1: { descricao: '1 Suco e 1 Sanduíche', valor: 9.50, extra: false },
+            combo2: { descricao: '1 Café e 1 Sanduíche', valor: 7.50, extra: false },
         };
 
-        // Definir os descontos e acréscimos
-        const descontoDinheiro = 0.05;
-        const acrescimoCredito = 0.03;
+        // Definição das taxas e descontos
+        this.descontoDinheiro = 0.05;
+        this.acrescimoCredito = 0.03;
+    }
 
-        // Inicializar o valor total da compra
+    // Método para calcular o valor da compra
+    calcularValorDaCompra(formaDePagamento, itens) {
         let valorTotal = 0;
-
-        // Inicializar a variável para verificar se um item extra é pedido sem o principal
         let principalEncontrado = false;
 
-        // Verificar se a forma de pagamento é válida
-        if (formaDePagamento !== 'dinheiro' && formaDePagamento !== 'debito' && formaDePagamento !== 'credito') {
+        // Verificação da forma de pagamento válida
+        if (!['dinheiro', 'debito', 'credito'].includes(formaDePagamento)) {
             return "Forma de pagamento inválida!";
         }
 
-        // Percorrer os itens
+        // Para rastrear os extras e suas quantidades
+        const extras = {};
+        // Para rastrear os itens principais e suas quantidades
+        const itensPrincipais = {};
+
         for (const item of itens) {
             const [codigo, quantidade] = item.split(',');
-            const valorItem = cardapio[codigo];
+            const cardapioItem = this.cardapio[codigo];
 
-            // Verificar se o código do item é válido
-            if (!valorItem) {
+            // Verificação de item inválido
+            if (!cardapioItem) {
                 return "Item inválido!";
             }
 
-            // Verificar se é um item principal
-            if (!codigo.startsWith('combo') && !codigo.endsWith('extra')) {
+            // Verificação de item principal
+            if (!codigo.startsWith('combo')) {
                 principalEncontrado = true;
             }
 
-            // Calcular o valor total do item
-            valorTotal += valorItem * parseInt(quantidade);
+            // Verificação de quantidade inválida
+            if (isNaN(parseInt(quantidade)) || parseInt(quantidade) < 1) {
+                return "Quantidade inválida!";
+            }
+
+            // Cálculo do valor total
+            valorTotal += cardapioItem.valor * parseInt(quantidade);
+
+            // Rastreamento de itens extras
+            if (cardapioItem.extra) {
+                const itemPrincipal = codigo === 'chantily' ? 'cafe' : 'sanduiche';
+                extras[itemPrincipal] = extras[itemPrincipal]
+                    ? extras[itemPrincipal] + parseInt(quantidade)
+                    : parseInt(quantidade);
+            } else {
+                // Rastreamento de itens principais
+                itensPrincipais[codigo] = itensPrincipais[codigo]
+                    ? itensPrincipais[codigo] + parseInt(quantidade)
+                    : parseInt(quantidade);
+            }
         }
 
-        // Verificar se há itens no carrinho
+        // Verificação de carrinho vazio
         if (itens.length === 0) {
             return "Não há itens no carrinho de compra!";
         }
 
-        // Verificar se a quantidade de itens é válida
+        // Verificação de item extra sem principal
         if (!principalEncontrado) {
             return "Item extra não pode ser pedido sem o principal";
         }
 
-        // Aplicar desconto se for pagamento em dinheiro
+        // Verificação de itens extras sem principais correspondentes
+        for (const itemPrincipal in extras) {
+            const quantidadeExtra = extras[itemPrincipal];
+            if (!itensPrincipais[itemPrincipal] || quantidadeExtra > itensPrincipais[itemPrincipal]) {
+                return "Item extra não pode ser pedido sem o principal";
+            }
+        }
+
+        // Aplicação de desconto em dinheiro
         if (formaDePagamento === 'dinheiro') {
-            valorTotal *= (1 - descontoDinheiro);
+            valorTotal *= (1 - this.descontoDinheiro);
         }
 
-        // Aplicar acréscimo se for pagamento a crédito
+        // Aplicação de acréscimo no crédito
         if (formaDePagamento === 'credito') {
-            valorTotal *= (1 + acrescimoCredito);
+            valorTotal *= (1 + this.acrescimoCredito);
         }
 
-        // Formatar o valor total da compra
+        // Formatação do valor total
         const valorFormatado = valorTotal.toFixed(2).replace('.', ',');
 
         return `R$ ${valorFormatado}`;
--- src/caixa-da-lanchonete.test.js
+++ src/caixa-da-lanchonete.test.js
@@ -48,4 +48,28 @@ describe('CaixaDaLanchonete', () => {
         ['queijo com outro item', 'debito', 'Item extra não pode ser pedido sem o principal', ['cafe,1', 'queijo,1']],
     ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
         validaTeste(formaDePagamento, resultadoEsperado, itens));
+
+    //novos testes
+    test.each([
+        ['com quantidade zero', 'dinheiro', 'Quantidade inválida!', ['cafe,0']],
+        ['com um valor', 'credito', 'Item inválido!', ['1']],
+        ['com código inexistente', 'debito', 'Item inválido!', ['pizza, 1']],
+        ['com forma de pagamento inválida', 'especie', 'Forma de pagamento inválida!', ['cafe, 1']],
+    ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
+        validaTeste(formaDePagamento, resultadoEsperado, itens));
+
+    test.each([
+        ['chantily', 'dinheiro', 'Item extra não pode ser pedido sem o principal', ['chantily,1']],
+        ['queijo', 'credito', 'Item extra não pode ser pedido sem o principal', ['queijo,1']],
+        ['chantily com outro item', 'credito', 'Item extra não pode ser pedido sem o principal', ['chantily,1', 'sanduiche,1']],
+        ['queijo com outro item', 'debito', 'Item extra não pode ser pedido sem o principal', ['cafe,1', 'queijo,1']],
+    ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
+        validaTeste(formaDePagamento, resultadoEsperado, itens));
+
+    test.each([
+        ['compra de combo', 'credito', 'R$ 9,50', ['combo1,1']],
+        ['compra de combo', 'debito', 'R$ 7,50', ['combo2,1']],
+        ['compra de múltiplos combos', 'dinheiro', 'R$ 36,00', ['combo1,2', 'combo2,3']],
+    ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
+        validaTeste(formaDePagamento, resultadoEsperado, itens));
 });
